70546607a1a311feb4fd6c9f635f46ba2d089396
Movie World <100509655+ATHIF-EFX@users.noreply.github.com>
2022-04-07 04:49:23 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -63,7 +63,7 @@ async def next_page(bot, query):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"🎥[{get_size(file.file_size)}]📁 {file.file_name}", callback_data=f'files#{file.file_id}'
+                    text=f"🎥[{get_size(file.file_size)}] {file.file_name}", callback_data=f'files#{file.file_id}'
                 ),
             ]
             for file in files
