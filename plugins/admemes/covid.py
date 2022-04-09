04d5c904194adc71573c39567959a2e41c0fdcba
Movie World <100509655+ATHIF-EFX@users.noreply.github.com>
2022-04-09 15:43:40 +0530

    Update covid.py

--- plugins/admemes/covid.py
+++ plugins/admemes/covid.py
@@ -12,7 +12,7 @@ BUTTONS = InlineKeyboardMarkup([[InlineKeyboardButton("𝙲𝙻𝙾𝚂𝙴", ca
 async def reply_info(client, message):
     query = message.text.split(None, 1)[1]
     await message.reply_photo(
-        photo="https://telegra.ph/file/1b837a8df2670b0097aaf.jpg",
+        photo="https://telegra.ph/file/683e7a359da773b5a8fc2.jpg",
         caption=covid_info(query),
         quote=True
     )
